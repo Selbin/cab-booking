c10a0e6bc581234aadb6a6f3daa1769acdec4419
Selbin_C_R <selbinzr@gmail.com>
2020-09-08 19:38:44 +0530

    added endtrip handler

--- controller/tripController.js
+++ controller/tripController.js
@@ -2,7 +2,8 @@ const { exeQuery } = require('../database/database')
 const {
   calculateDistance,
   findNearestCab,
-  setResponseObj
+  setResponseObj,
+  calculateCost
 } = require('../helper/helper')
 
 const bookCab = async (req, res) => {
@@ -15,10 +16,17 @@ const bookCab = async (req, res) => {
   }
   const query1 = 'select * from cabs where available = $1 ' + str
   const query2 = 'update cabs set available = $1 where cab_id = $2'
-  const query3 = 'insert into trip(cab_id, user_id, lat, lon, end_lat, end_lon, cost) values ($1, $2, $3, $4, $5, $6, $7) returning trip_id'
+  const query3 =
+    'insert into trip(cab_id, user_id, lat, lon, start_time, end_time, end_lat, end_lon, cost) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning trip_id'
   try {
     let result = await exeQuery(query1, value1)
-    if (!result.rowCount) return res.status(400).json(setResponseObj(false, null, 'Cab unavailable. Please try again later'))
+    if (!result.rowCount) {
+      return res
+        .status(404)
+        .json(
+          setResponseObj(false, null, 'Cab unavailable. Please try again later')
+        )
+    }
     let cabObj = {
       cabInfo: result.rows[0],
       distance: calculateDistance(
@@ -39,7 +47,17 @@ const bookCab = async (req, res) => {
     }
     if (cabObj.cabInfo.cab_id) {
       await exeQuery(query2, [false, cabObj.cabInfo.cab_id])
-      result = await exeQuery(query3, [cabObj.cabInfo.cab_id, userId, lat, lon, 0, 0, 0])
+      result = await exeQuery(query3, [
+        cabObj.cabInfo.cab_id,
+        userId,
+        lat,
+        lon,
+        new Date(),
+        new Date(),
+        0,
+        0,
+        0
+      ])
       cabObj.tripId = result.rows[0].trip_id
     }
     res.status(200).json(setResponseObj(true, cabObj, 'Booking successful'))
@@ -49,4 +67,32 @@ const bookCab = async (req, res) => {
   }
 }
 
-module.exports = { bookCab }
+const endTrip = async (req, res) => {
+  const { endLat, endLon, tripId } = req.params
+  const query1 =
+    'select trip.*, cabs.color from trip inner join cabs on cabs.cab_id = trip.cab_id where trip_id = $1'
+  const query2 = 'update cabs set available = $1, lat = $3, lon= $4 where cab_id = $2'
+  const query3 = 'update trip set end_lat =$1, end_lon=$2, end_time=$3, cost=$4 where trip_id = $5 returning *'
+  try {
+    let result = await exeQuery(query1, [tripId])
+    if (!result.rowCount) {
+      return res.status(404).json(setResponseObj(false, null, 'Trip not found'))
+    }
+    await exeQuery(query2, [true, result.rows[0].cab_id, endLat, endLon])
+    const distanceTravelled = calculateDistance(
+      endLat,
+      endLon,
+      result.rows[0].lat,
+      result.rows[0].lon
+    )
+    const travelCost = calculateCost(distanceTravelled, result.rows[0].start_time, new Date(), result.rows[0].color)
+    await exeQuery(query2, [true, result.rows[0].cab_id])
+    result = await exeQuery(query3, [endLat, endLon, new Date(), travelCost, tripId])
+    res.status(200).json(setResponseObj(true, result.rows[0], 'Trip completed'))
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(setResponseObj(false, null, 'Internal server error'))
+  }
+}
+
+module.exports = { bookCab, endTrip }
