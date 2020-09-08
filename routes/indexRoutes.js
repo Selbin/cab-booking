e806a884a2f5dcda7d7b84d8023a9920751d11aa
Selbin_C_R <selbinzr@gmail.com>
2020-09-08 19:39:22 +0530

    added route for ending trip

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const tripController = require('../controller/tripController')
 
 router.get('/book/:lat/:lon/:userId/:color', tripController.bookCab)
+router.put('/endTrip/:tripId/:endLat/:endLon', tripController.endTrip)
 
 module.exports = router
