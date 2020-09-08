11a478f25a5cd57bbb83ce3ecf894dd8a5622b2b
Selbin_C_R <selbinzr@gmail.com>
2020-09-08 17:49:54 +0530

    added base url

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const dotEnv = require('dotenv')
+const indexRoutes = require('./routes/indexRoutes')
 
 dotEnv.config()
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/fuber', indexRoutes)
 
 app.listen(process.env.APP_PORT, () => console.log('listening to: ', process.env.APP_PORT))
 
