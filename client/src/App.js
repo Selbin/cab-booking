0383b7bb788a84844cb1f257813a186d3c7dfa07
Selbin_C_R <selbinzr@gmail.com>
2020-09-11 10:04:26 +0530

    refactored code

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import MapComponent from './component/MapComponent'
-const io = require('socket.io-client')
+import io from 'socket.io-client'
 const socket = io('http://localhost:8000')
 let tempMarker = ''
 
